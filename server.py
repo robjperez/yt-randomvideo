4a3abdd31ad0429c25a6a90d445bac29b4e07a0a
Roberto Perez <robj.perez@gmail.com>
2024-04-18 15:39:47 +0200

    Add TV category

--- server.py
+++ server.py
@@ -33,6 +33,22 @@ def get_random_channel(channel_id):
     random_video = result_lines[random.randint(0, len(result_lines))]
     return json.dumps({'url': YT_BASE_URL + random_video })
 
+from random import choice
+
+@app.route('/random', methods=['GET'])
+def get_random_video():
+    channel_id = choice(list(CHANNELS.keys()))
+    if channel_id in CACHE and len(CACHE[channel_id]) > 0:
+        result_lines = CACHE[channel_id]
+    else:
+        channel_url = CHANNELS[channel_id]
+        result = subprocess.run(['yt-dlp', '--flat-playlist', '--print', 'id', channel_url], stdout=subprocess.PIPE)
+        result_lines = result.stdout.decode('utf-8').split('\n')
+        CACHE[channel_id] = result_lines
+
+    random_video = result_lines[random.randint(0, len(result_lines))]
+    return json.dumps({'url': YT_BASE_URL + random_video })
+
 @app.route('/', methods=['GET'])
 def get_web_app():
     return render_template('index.html')
